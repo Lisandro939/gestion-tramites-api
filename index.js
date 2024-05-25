f492f9bc0da46bdbd83186cc583ad3edf64acc47
lisandrofernandez2705 <lisandrofernandez2705@gmail.com>
2024-05-25 21:14:12 +0000

    feat: getUserByCUIL endpoint

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = process.env.PORT || process.argv[3] || 8081;
 
+const users = require("./data/users.json")
+
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs');
@@ -14,7 +16,33 @@ app.get('/views', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.json({"msg": "Hello world"});
+  console.log(req)
+  res.json({ "msg": "Hello world" });
+});
+
+app.get('/CUIL/:CUIL', (req, res) => {
+  const { CUIL } = req.params;
+  const user = users.find((user) => user.CUITCliente === parseInt(CUIL))
+  if (!!user) {
+    res.json(
+      {
+        "status": "200",
+        "msg": `The CUIL ${CUIL} is correct`,
+        "user": {
+          "name": user.nombreCliente,
+          "CUIL": user.CUITCliente,
+          "address": user.direccionCliente
+        }
+      }
+    );
+  } else {
+    res.json(
+      {
+        "status": "400",
+        "msg": `User with CUIL ${CUIL} not found`,
+      }
+    );
+  }
 });
 
 app.listen(port, () => {
