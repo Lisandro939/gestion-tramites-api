b3fac76d2087f6fdef84a1d57e115abb331ae70d
lisandrofernandez2705 <lisandrofernandez2705@gmail.com>
2024-05-25 20:11:38 +0000

    fix: JSON o "/"

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/views', (req, res) => {
   res.render('index');
 });
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.json({"msg": "Hello world"});
 });
 
